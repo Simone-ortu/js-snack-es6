1d223320bd83a7cbf237bcee41d5b8611537d4b3
Simone-Ortu <simo.28.09o@gmail.com>
2021-01-19 15:31:18 +0100

    Es 1 finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // 1) Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 let numInsert = parseInt(prompt('Inserisci un numero'))
+
+if (numInsert %2 ==0) {
+    console.log(numInsert);
+} else {
+    console.log(numInsert+1);
+}
