7a53fbd1ed4eed7ee6630b016b1ea820630f5c8f
Simone-Ortu <simo.28.09o@gmail.com>
2021-01-20 15:27:21 +0100

    Es 1fine

--- es20-01/js/script.js
+++ es20-01/js/script.js
@@ -1,63 +1,35 @@
-// 1) Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+// jsnack 1
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
 
-// const numInsert = parseInt(prompt('Inserisci un numero'))
+const bici = [
+    {
+        nome:'city bike',
+        peso: 60
+    },
+    {
+        nome: 'downhill',
+        peso: 65
+    },
+    {
+        nome: 'bici da corsa',
+        peso: 30
+    },
+    {
+        nome: 'BMX',
+        peso: 25
+    },
+]
 
-// if (numInsert %2 ==0) {
-//     console.log(numInsert);
-// } else {
-//     console.log(numInsert+1);
-// }
-// Utilizzando const e/o let e template literal. Creare due tag div con due id diversi. 
-// Un div avrà il testo colorato di rosso mentre l’altro di verde.
-// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const [bici1, bici2, bici3, bici4] = bici
+let biciDefault = bici1.peso
 
-const num = [];
-let numOdd = 'red'
-let numEven = 'green'
-let evenNum = [];
-let oddNum = [];
-for (let i = 0; i < 10; i++) {
-    num.push(genaraRnd(10, 1))
-    if (num[i] % 2 == 0) {
-        evenNum.push(num[i])
-    }
-    else {
-        oddNum.push(num[i])
-    }
+
+if (bici2.peso < biciDefault) {
+    biciDefault = bici2
+} else if (bici3.peso < biciDefault){
+    biciDefault = bici3
+}else if (bici4.peso < biciDefault) {
+    biciDefault = bici4
 }
-
-console.log(num);
-console.log(evenNum);
-console.log(oddNum);
-
-
-document.getElementById('numeriP').innerHTML = `<p class="${numEven}">${evenNum}</p>`
-
-document.getElementById('numeriD').innerHTML = `<p class="${numOdd}">${oddNum}</p>`
-
-
-
-
-function genaraRnd() {
-    return Math.floor(Math.random() * (10 - 1)) + 1;
-}
-
-
-//   3) Utilizzando const e/o let. Dare la possibilità di inserire due parole.
-//    Verificare se le due parole hanno la stessa lunghezza. Se hanno la stessa lunghezza, 
-//   stamparle entrambe altrimenti stampare la più lunga delle due.
-
-const primaParola = prompt('Inserire una parola')
-const secondaParola = prompt('Inserire una parola')
-
-if (primaParola.length > secondaParola.length) {
-    console.log(primaParola);
-}else if (primaParola.length == secondaParola.length) {
-    console.log(primaParola + ' '+ secondaParola);
-}else{
-    console.log(secondaParola);
-}
-    
-
-
-
+console.log(`La bici che pesa meno é :${biciDefault.nome},il suo peso é :${biciDefault.peso}`);
